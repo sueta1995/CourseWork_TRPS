115f8f101e5fda690edfe1f164310e7a5cadaa67
sueta1995 <kitaev.tagir@gmail.com>
2023-08-06 00:22:11 +0300

    Добавлены переменные окружения

--- sueta/sueta/settings.py
+++ sueta/sueta/settings.py
@@ -12,8 +12,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Загрузка переменных окружения
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
@@ -77,9 +83,9 @@ WSGI_APPLICATION = 'sueta.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'odonata_ai',
-        'USER': 'sueta1995',
-        'PASSWORD': 't46BOY003kit',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '',
     }
