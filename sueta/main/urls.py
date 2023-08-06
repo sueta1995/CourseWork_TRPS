77ed84abef4f1ee2d612c467c892a1ce849adcba
sueta1995 <kitaev.tagir@gmail.com>
2023-08-06 16:27:16 +0300

    изменения в интерфейсе

--- sueta/main/urls.py
+++ sueta/main/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:find_id>/', views.detail, name='detail')
 ]
--- sueta/main/views.py
+++ sueta/main/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from main.models import Find
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Пиздец")
+
+def detail(request, find_id):
+    find = Find.objects.get(id=find_id)
+    response = f'Ты смотришь находку с видом {find.dragonfly.common_name}'
+
+    return HttpResponse(response)
