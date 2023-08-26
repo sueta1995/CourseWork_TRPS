50eb6c3042279f611a124bc6df80ba94a74e05a5
sueta1995 <kitaev.tagir@gmail.com>
2023-08-26 22:18:16 +0300

    добавлена карта для отображения находок и зон

--- sueta/finds/templatetags/finds_tags.py
+++ sueta/finds/templatetags/finds_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from finds.models import *
 
+import os
+
 
 register = template.Library()
 
@@ -11,3 +13,7 @@ def get_finds():
 @register.simple_tag()
 def get_dragonflies():
     return Dragonfly.objects.all()
+
+@register.simple_tag()
+def get_env(key):
+    return os.environ.get(key)
